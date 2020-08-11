e889b2b597cf39eed91c286b847343b65b0411e3
MarharytaKozachok <marharyta.kozachok@gmail.com>
2020-08-11 23:03:58 +0300

    Added departments filter

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -10,8 +10,16 @@ class EmployeeController extends Controller
 {
     public function index(Request $request)
     {
+        $employees = Employee::query();
+        if ($departmentId = $request->get('departmentId')) {
+            $employees->where('department_id', $departmentId);
+        }
+
+        $employees = $employees->paginate($request->get('perPage', 10));
+
         return View::make('employee', [
-            'employees' => Employee::paginate($request->get('perPage', 10)),
+            'employees' => $employees,
         ]);
     }
+
 }
--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -3,13 +3,18 @@
 @section('content')
     <div class="main-content">
         <div class="row">
-            <div class="article col-9">
+            <div class="article col-12">
                 @foreach($employees as $employee)
                     <div class="employee">
                         <h3>{{ $employee->full_name }}</h3>
                         <ul>
                             <li><strong> Birth date:</strong> {{ $employee->birth_date->format('d.m.Y') }}</li>
-                            <li><strong> Department:</strong> {{ $employee->department->name }}</li>
+                            <li>
+                                <strong> Department:</strong>
+                                <a href="{{ route('employee-index', ['departmentId' => $employee->department->id] + request()->all()) }}">
+                                    {{ $employee->department->name }}
+                                </a>
+                            </li>
                             <li><strong> Position:</strong> {{ $employee->position }} </li>
                             <li><strong> Employment type:</strong> {{ $employee->employment_type }} </li>
                             <li><strong> Month Salary:</strong> {{ $employee->getSalary() }} </li>
@@ -19,9 +24,9 @@
             </div>
 
         </div>
-        <div class="row">
+        <div class="row col-12">
             <div class="col-2">
-                <form action="{{ route('employee-index') }}" method="GET">
+                <form action="{{ route('employee-index', request()->all()) }}" method="GET">
                     <select class="form-control" name="perPage" onchange="this.form.submit()">
                         <option {{ request('perPage') == 10 ? 'selected' : '' }}>10</option>
                         <option {{ request('perPage') == 25 ? 'selected' : '' }}>25</option>
@@ -31,7 +36,7 @@
                 </form>
             </div>
             <div class="col-10">
-                {{ $employees->appends(request()->only(['page', 'perPage']))->render() }}
+                {{ $employees->appends(request()->only(['departmentId', 'page', 'perPage']))->render() }}
             </div>
         </div>
     </div>
